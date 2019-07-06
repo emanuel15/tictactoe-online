1679108d32a1f0b52a7df30220e0af1b90c3345a
Emanuel Pimentel <emanuel_pimentel@outlook.com>
2019-07-06 14:39:33 -0300

    fixed port

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const WebSocket = require('ws');
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
